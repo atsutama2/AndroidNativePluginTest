6add2b87f20c9b717def9b5d34a01451e9885f8d
atsutama2 <atsutama.0713@gmail.com>
2021-11-16 18:59:55 +0900

    targetSdkVersion変更、音声認識のコード修正

--- NativeSpeech/src/main/java/jp/co/test/nativespeechrecognizer/NativeSpeechRecognizer.java
+++ NativeSpeech/src/main/java/jp/co/test/nativespeechrecognizer/NativeSpeechRecognizer.java
@@ -7,15 +7,20 @@ import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
 import android.content.Intent;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Locale;
+
 import com.unity3d.player.UnityPlayerActivity;
 
 import static com.unity3d.player.UnityPlayer.UnitySendMessage;
 
-public class NativeSpeechRecognizer {
+public class NativeSpeechRecognizer extends UnityPlayerActivity {
+
+    private static final String TAG = "LOGTAG: ";
+
     public static void staticFunction(final String callbackTarget, final String callbackMethod){
-        Log.d("デバッグ1：", callbackTarget ) ;
         Log.e("[Unity Test]","Static Method has been called!");
         UnitySendMessage(callbackTarget, callbackMethod,"はい！");
     }
@@ -25,75 +30,128 @@ public class NativeSpeechRecognizer {
         UnitySendMessage(callbackTarget, callbackMethod,"いいえ！");
     }
 
-    public static void StartRecognizer(Context context, final String callbackTarget, final String callbackMethod)
+    public static void StartRecognizer(final Context context, final String callbackTarget, final String callbackMethod)
     {
-        Log.e("デバッグ1： ", callbackTarget ) ;
-        Log.e("デバッグ2： ", callbackMethod ) ;
 
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        Log.e("デバッグ3： ", callbackTarget ) ;
-
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        Log.e("デバッグ4： ", callbackMethod ) ;
         intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, context.getPackageName());
-        Log.e("デバッグ5： ", callbackMethod ) ;
 
-        SpeechRecognizer recognizer = SpeechRecognizer.createSpeechRecognizer(context);
-        Log.e("デバッグ6： ", callbackMethod ) ;
+        //言語設定
+        String lang = "ja-JP";
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, lang);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_PREFERENCE, lang);
+        intent.putExtra(RecognizerIntent.EXTRA_ONLY_RETURN_LANGUAGE_PREFERENCE, lang);
+        intent.putExtra(RecognizerIntent.EXTRA_RESULTS_PENDINGINTENT, lang);
+
+        final SpeechRecognizer recognizer = SpeechRecognizer.createSpeechRecognizer(context);
         recognizer.setRecognitionListener(new RecognitionListener()
         {
             @Override
-            public void onReadyForSpeech(Bundle params)
+            public void onReadyForSpeech(Bundle bundle)
             {
-                Log.e("デバッグ8： ", callbackMethod ) ;
-                // On Ready for speech.
+                // 音声認識の準備ができた時呼び出される
+                Log.d(TAG, "onReadyForSpeech:" + bundle.toString());
                 UnitySendMessage(callbackTarget, callbackMethod, "onReadyForSpeech");
             }
 
             @Override
             public void onBeginningOfSpeech()
             {
-                Log.e("デバッグ9： ", callbackMethod ) ;
-                // On begining of speech.
+                // マイクに向かってしゃべり始めると呼び出される
+                Log.d(TAG, "onBeginningOfSpeech:");
                 UnitySendMessage(callbackTarget, callbackMethod, "OnBeginningOfSpheech");
             }
 
             @Override
             public void onRmsChanged(float rmsdB)
             {
-                Log.e("デバッグ10： ", callbackMethod ) ;
                 // On Rms changed.
+                Log.d(TAG, "ボリューム" + rmsdB);
                 UnitySendMessage(callbackTarget, callbackMethod, "onRmsChanged");
             }
 
             @Override
             public void onBufferReceived(byte[] buffer)
             {
-                Log.e("デバッグ11： ", callbackMethod ) ;
                 // On buffer received.
+                Log.d(TAG, "音声データ");
                 UnitySendMessage(callbackTarget, callbackMethod, "onBufferReceived");
             }
 
             @Override
             public void onEndOfSpeech()
             {
-                Log.e("デバッグ12： ", callbackMethod ) ;
                 // On end of speech.
+                Log.d(TAG, "onEndOfSpeech");
                 UnitySendMessage(callbackTarget, callbackMethod, "onEndOfSpeech");
             }
 
             @Override
             public void onError(int error)
             {
-                Log.e("デバッグ13： ", callbackMethod ) ;
+                String errorMessage;
+
+                switch (error) {
+                    case SpeechRecognizer.ERROR_AUDIO:
+                        // 音声データ保存失敗
+                        Log.d(TAG, "ERROR_AUDIO");
+                        errorMessage = "Audio recording error";
+                        break;
+                    case SpeechRecognizer.ERROR_CLIENT:
+                        // Android端末内のエラー(その他)
+                        Log.d(TAG, "ERROR_CLIENT");
+                        errorMessage = "Client side error";
+                        break;
+                    case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
+                        // 権限無し
+                        Log.e(TAG, "ERROR_INSUFFICIENT_PERMISSIONS");
+                        errorMessage = "Insufficient permissions";
+                        break;
+                    case SpeechRecognizer.ERROR_NETWORK:
+                        // ネットワークエラー(その他)
+                        Log.e(TAG, "ERROR_NETWORK");
+                        errorMessage = "Network related error";
+                        break;
+                    case SpeechRecognizer.ERROR_NETWORK_TIMEOUT:
+                        // ネットワークタイムアウトエラー
+                        Log.e(TAG, "ERROR_NETWORK_TIMEOUT");
+                        errorMessage = "Network operation timeout";
+                        break;
+                    case SpeechRecognizer.ERROR_NO_MATCH:
+                        // 音声認識結果無し
+                        Log.e(TAG, "ERROR_NO_MATCH");
+                        errorMessage = "No matching phrase";
+                        break;
+                    case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
+                        // RecognitionServiceへ要求出せず
+                        Log.e(TAG, "ERROR_RECOGNIZER_BUSY");
+                        errorMessage = "RecognitionServiceBusy";
+                        break;
+                    case SpeechRecognizer.ERROR_SERVER:
+                        // Server側からエラー通知
+                        Log.e(TAG, "ERROR_SERVER");
+                        errorMessage = "Server sends error status";
+                        break;
+                    case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
+                        // 音声入力無し
+                        Log.e(TAG, "ERROR_SPEECH_TIMEOUT");
+                        errorMessage = "No input detected";
+                        break;
+                    default:
+                        errorMessage = "ASR error";
+                        break;
+                }
                 // On error.
                 UnitySendMessage(callbackTarget, callbackMethod, "onError");
+
+                Toast.makeText(context, errorMessage, Toast.LENGTH_LONG).show();
             }
 
             @Override
             public void onResults(Bundle results)
             {
-                Log.e("デバッグ14： ", callbackMethod ) ;
+                Log.e(TAG, "onResults") ;
                 ArrayList<String> list = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
                 String str = "";
                 for (String s : list)
@@ -104,14 +162,14 @@ public class NativeSpeechRecognizer {
                     }
                     str += s;
                 }
-
+                Log.e(TAG, str ) ;
                 UnitySendMessage(callbackTarget, callbackMethod, "onResults\n" + str);
             }
 
             @Override
             public void onPartialResults(Bundle partialResults)
             {
-                Log.e("デバッグ15： ", callbackMethod ) ;
+                Log.e(TAG, "onPartialResults" ) ;
                 // On partial results.
                 UnitySendMessage(callbackTarget, callbackMethod, "onPartialResults");
             }
@@ -119,7 +177,7 @@ public class NativeSpeechRecognizer {
             @Override
             public void onEvent(int eventType, Bundle params)
             {
-                Log.e("デバッグ16： ", callbackMethod ) ;
+                Log.e(TAG, "onEvent" ) ;
                 // On event.
                 UnitySendMessage(callbackTarget, callbackMethod, "onEvent");
             }
